738653bea90665c33b19d1c3f1a31c3af21c548b
Aaron Sayson <saysonaaronmark@gmail.com>
2025-08-21 15:37:00 +0800

    feat: investment interests calculated and shown in the result table if all input fields have values

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import Header from "./components/Header"
-import ResultsTable from "./components/ResultsTable"
-import UserInputs from "./components/UserInputs"
+import { useState } from "react";
+import Header from "./components/Header";
+import ResultsTable from "./components/ResultsTable";
+import NumInput from "./components/NumInput";
+
+const INITIAL_INVESTMENT_FACTORS = {
+  initialInvestment: '',
+  annualInvestment: '',
+  expectedReturn: '',
+  duration: '',
+};
 
 function App() {
+  const [investmentFactors, setInvestmentFactors] = useState(
+    INITIAL_INVESTMENT_FACTORS
+  );
+
+  const handleChangeInput = (identifier, newValue) => {
+    setInvestmentFactors(prevFactors => ({
+      ...prevFactors,
+      [identifier]: Number(newValue)
+    }))
+  }
+
   return (
     <main>
       <Header />
-      <UserInputs />
-      <ResultsTable />
+      <div id="user-input">
+        <div className="input-group">
+          <NumInput id="initialInvestment" label="Initial Investment" onUpdateInvFactor={handleChangeInput} />
+          <NumInput id="annualInvestment" label="Annual Investment" onUpdateInvFactor={handleChangeInput} />
+        </div>
+        <br />
+        <div className="input-group">
+          <NumInput id="expectedReturn" label="Expected Return" onUpdateInvFactor={handleChangeInput} />
+          <NumInput id="duration" label="Duration" onUpdateInvFactor={handleChangeInput} />
+        </div>
+      </div>
+      <ResultsTable calculationParameters={investmentFactors} />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ResultsTable.jsx
+++ src/components/ResultsTable.jsx
@@ -1,26 +1,56 @@
-const ResultsTable = () => {
+import { calculateInvestmentResults, formatter } from "../util/investment.js";
+
+const isFullyPopulated = (obj) => Object.values(obj).every(parameter => parameter)
+
+const noDataFallback = (
+  <tr>
+    <td colSpan={5} className="center">
+      <small>
+        <em>Enter value to all required fields...</em>
+      </small>
+    </td>
+  </tr>
+);
+
+const ResultsTable = ({ calculationParameters }) => {
+  const { initialInvestment } =
+    calculationParameters;
+  let investmentInterests;
+
+  if (isFullyPopulated(calculationParameters)) {
+    investmentInterests = calculateInvestmentResults(calculationParameters);
+  }
+
   return (
     <table id="result">
-        <thead>
-            <tr>
-                <th>Year</th>
-                <th>Investment Value</th>
-                <th>Interest (Year)</th>
-                <th>Total Interest</th>
-                <th>Invested Capital</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>1</td>
-                <td>$10,000</td>
-                <td>$809</td>
-                <td>$890</td>
-                <td>$20,111</td>
-            </tr>
-        </tbody>
-    </table>
-  )
-}
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {investmentInterests &&
+          investmentInterests.map((rows, index) => {
+            const investedCapital = initialInvestment + (rows.annualInvestment * rows.year);
 
-export default ResultsTable
+            return (
+            <tr key={index}>
+              <td>{rows.year}</td>
+              <td>{formatter.format(rows.valueEndOfYear)}</td>
+              <td>{formatter.format(rows.interest)}</td>
+              <td>{formatter.format(rows.valueEndOfYear - investedCapital)}</td>
+              <td>{formatter.format(investedCapital)}</td>
+            </tr>
+          )
+          })}
+        {!investmentInterests && noDataFallback}
+      </tbody>
+    </table>
+  );
+};
+
+export default ResultsTable;
